a2e68956282d931daaedd08008fba952aa87f891
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-29 22:41:21 +0530

    Display masssege

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList");
+        } 
+    }
+}
